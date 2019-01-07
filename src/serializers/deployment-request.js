22520c647f51f2877723272783e4940e6ebf5786
Arnaud Valensi <arnaud.valensi@gmail.com>
2019-01-07 16:00:35 +0100

    Fix environments:copy-layout command

--- src/serializers/deployment-request.js
+++ src/serializers/deployment-request.js
@@ -1,14 +1,6 @@
 const JSONAPISerializer = require('jsonapi-serializer').Serializer;
 
 module.exports = new JSONAPISerializer('deployment-requests', {
-  attributes: ['from', 'to'],
-  from: {
-    ref: 'id',
-    included: false,
-  },
-  to: {
-    ref: 'id',
-    included: false,
-  },
-  typeForAttribute: () => 'environments',
+  attributes: ['type', 'from', 'to'],
+  typeForAttribute: () => 'deployment-requests',
 });
