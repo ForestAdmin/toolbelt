2b12fc78f23b110f5b0bbe294d0696ea3ffd40b3
David Routhieau <rootio@protonmail.com>
2019-01-09 17:16:38 +0100

    Review fixes

--- src/commands/environments/copy-layout.js
+++ src/commands/environments/copy-layout.js
@@ -4,7 +4,6 @@ const chalk = require('chalk');
 const inquirer = require('inquirer');
 const EnvironmentManager = require('../../services/environment-manager');
 const Prompter = require('../../services/prompter');
-const logger = require('../../services/logger');
 
 class CopyLayoutCommand extends Command {
   async run() {
@@ -21,14 +20,14 @@ class CopyLayoutCommand extends Command {
     try {
       fromEnvironment = await manager.getEnvironment(config.fromEnvironment);
     } catch (err) {
-      logger.error(`Cannot find the source environment ${chalk.bold(config.fromEnvironment)} on the project ${chalk.bold(config.projectId)}.`);
+      this.error(`Cannot find the source environment ${chalk.bold(config.fromEnvironment)} on the project ${chalk.bold(config.projectId)}.`);
       process.exit(3);
     }
 
     try {
       toEnvironment = await manager.getEnvironment(config.toEnvironment);
     } catch (err) {
-      logger.error(`Cannot find the target environment ${chalk.bold(config.toEnvironment)} on the project ${chalk.bold(config.projectId)}.`);
+      this.error(`Cannot find the target environment ${chalk.bold(config.toEnvironment)} on the project ${chalk.bold(config.projectId)}.`);
       process.exit(3);
     }
 
@@ -42,14 +41,21 @@ class CopyLayoutCommand extends Command {
         }]);
     }
 
-    if (!answers || answers.confirm === toEnvironment.name) {
-      const copyLayout = await manager.copyLayout(fromEnvironment.id, toEnvironment.id);
-      if (copyLayout) {
-        return this.log(`Environment's layout ${chalk.red(fromEnvironment.name)} successfully copied to ${chalk.red(toEnvironment.name)}.`);
+    try {
+      if (!answers || answers.confirm === toEnvironment.name) {
+        const copyLayout = await manager.copyLayout(fromEnvironment.id, toEnvironment.id);
+        if (copyLayout) {
+          return this.log(`Environment's layout ${chalk.red(fromEnvironment.name)} successfully copied to ${chalk.red(toEnvironment.name)}.`);
+        }
+        return this.error('💀  Oops, something went wrong.💀', { exit: 1 });
       }
-      return this.error('💀  Oops, something went wrong.💀', { exit: 1 });
+      return this.error(`Confirmation did not match ${chalk.red(toEnvironment.name)}. Aborted.`, { exit: 2 });
+    } catch (error) {
+      if (error.status === 403) {
+        return this.error(`You do not have the rights to copy the layout of the environment ${chalk.bold(fromEnvironment.name)} to ${chalk.bold(toEnvironment.name)}.`, { exit: 1 });
+      }
+      return this.error(error, { exit: 1 });
     }
-    return this.error(`Confirmation did not match ${chalk.red(toEnvironment.name)}. Aborted.`, { exit: 2 });
   }
 }
 
