680b3f017d2f37ddb314c717acc3c3f803ae9b84
Arnaud Valensi <arnaud.valensi@gmail.com>
2019-01-10 10:47:07 +0100

    Fix based on review

--- src/serializers/deployment-request.js
+++ src/serializers/deployment-request.js
@@ -2,5 +2,4 @@ const JSONAPISerializer = require('jsonapi-serializer').Serializer;
 
 module.exports = new JSONAPISerializer('deployment-requests', {
   attributes: ['type', 'from', 'to'],
-  typeForAttribute: () => 'deployment-requests',
 });
