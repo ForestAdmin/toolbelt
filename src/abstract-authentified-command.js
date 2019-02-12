828af8b6e3a97d0aedacb7b6219acd9f90403121
Vincent Molinié <vincent.molinie@epita.fr>
2019-02-12 17:29:49 +0100

    Fix error of project not found

--- src/abstract-authentified-command.js
+++ src/abstract-authentified-command.js
@@ -1,13 +1,24 @@
 const { Command } = require('@oclif/command');
 const authenticator = require('./services/authenticator');
+const chalk = require('chalk');
 
 class AbstractAuthentifiedCommand extends Command {
   async run() {
     if (!authenticator.getAuthToken()) {
-      return this.error('🔥  You need to be logged in to execute this command 🔥', { exit: 10 });
+      return this.error(`🔥  Please use ${chalk.bold('forest login')} to sign in to your Forest account. 🔥`, { exit: 10 });
     }
 
-    return this.runIfAuthentified();
+    try {
+      const result = await this.runIfAuthentified();
+      return result;
+    } catch (error) {
+      // NOTICE: Due to ip-whitelist, 404 will never be thrown for a project
+      if (error.status === 403) {
+        return this.error('💀  You do not have the right to execute this action on this project 💀');
+      }
+
+      throw error;
+    }
   }
 
   async runIfAuthentified() {
