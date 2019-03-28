35ede7d6e9ba8303d8d367011e9ce7437242cb9a
Sandro Munda <sandro@munda.me>
2019-03-28 14:49:16 +0100

    Add env variables to avoid prompt in the login command

--- src/services/prompter.js
+++ src/services/prompter.js
@@ -10,27 +10,35 @@ async function Prompter(requests) {
   const prompts = [];
 
   if (isRequested('email')) {
-    prompts.push({
-      type: 'input',
-      name: 'email',
-      message: 'What\'s your email address? ',
-      validate: (email) => {
-        if (email) { return true; }
-        return '🔥  Please enter your email address 🔥';
-      },
-    });
+    if (process.env.FOREST_EMAIL) {
+      envConfig.email = process.env.FOREST_EMAIL;
+    } else {
+      prompts.push({
+        type: 'input',
+        name: 'email',
+        message: 'What\'s your email address? ',
+        validate: (email) => {
+          if (email) { return true; }
+          return '🔥  Please enter your email address 🔥';
+        },
+      });
+    }
   }
 
   if (isRequested('password')) {
-    prompts.push({
-      type: 'password',
-      name: 'password',
-      message: 'What\'s your password: ',
-      validate: (password) => {
-        if (password) { return true; }
-        return '🔥  Oops, your password cannot be blank 🔥';
-      },
-    });
+    if (process.env.FOREST_PASSWORD) {
+      envConfig.password = process.env.FOREST_PASSWORD;
+    } else {
+      prompts.push({
+        type: 'password',
+        name: 'password',
+        message: 'What\'s your password: ',
+        validate: (password) => {
+          if (password) { return true; }
+          return '🔥  Oops, your password cannot be blank 🔥';
+        },
+      });
+    }
   }
 
   const config = await inquirer.prompt(prompts);
