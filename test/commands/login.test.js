057c3eab93a9e6529572df60d5338d61e50d8134
Sliman <smedini@gmail.com>
2020-03-17 16:15:11 +0100

    better test cli management.

--- test/commands/login.test.js
+++ test/commands/login.test.js
@@ -1,65 +1,66 @@
-const Nock = require('@fancy-test/nock').default;
-const { expect, test } = require('@oclif/test');
 const jwt = require('jsonwebtoken');
-const mockStdin = require('mock-stdin');
+const nock = require('nock');
 
-const input = (value, delay = 0) => ({
-  run: () => {
-    const stdin = mockStdin.stdin();
-    setTimeout(() => {
-      stdin.send(value);
-      stdin.end();
-    }, delay);
-  },
-});
-
-const fancy = test
-  .register('nock', Nock)
-  .register('input', input);
+const testCli = require('./test-cli');
+const LoginCommand = require('../../src/commands/login');
 
 describe('login', () => {
   describe('with trivial mail and bad token in args', () => {
-    fancy
-      .stdout()
-      .stderr()
-      .command(['login', '-e', 'smile@gmail.com', '-t', 'bad_token'])
-      .it('should warn about bad token', (ctx) => {
-        expect(ctx.stderr).to.contain('Invalid token. Please enter your authentication token.');
-      });
+    it('should login successful', () => testCli({
+      inputs: [
+        `${jwt.sign({}, 'key', { expiresIn: '1day' })}\n`,
+      ],
+      command: () => LoginCommand.run(['-e', 'smile@gmail.com', '-t', 'bad_token']),
+      errorOutputs: [
+        'Invalid token. Please enter your authentication token.',
+      ],
+    }));
   });
 
   describe('with trivial mail and valid token in args', () => {
-    fancy
-      .stdout()
-      .stderr()
-      .command(['login', '-e', 'smile@gmail.com', '-t', jwt.sign({}, 'key', { expiresIn: '1day' })])
-      .it('should display "Login successful"', (ctx) => {
-        expect(ctx.stdout).to.contain('Login successful');
-      });
-  });
-
-  describe('stdin test', () => {
-    fancy
-      .input('token')
-      .it('should contain data equals "token"', () => {
-        process.stdin.setEncoding('utf8');
-        process.stdin.once('data', (data) => {
-          expect(data).to.equals('token');
-        });
-      });
+    it('should login successful', () => testCli({
+      inputs: [
+        `${jwt.sign({}, 'key', { expiresIn: '1day' })}\n`,
+      ],
+      command: () => LoginCommand.run(['-e', 'smile@gmail.com', '-t', jwt.sign({}, 'key', { expiresIn: '1day' })]),
+      outputs: [
+        'Login successful',
+      ],
+    }));
   });
 
   describe('with a google mail and a valid token', () => {
-    fancy
-      .stdout({ print: true })
-      .env({ FOREST_URL: 'http://localhost:3001' })
-      .nock('http://localhost:3001', (api) => api
+    it('should login successful', () => testCli({
+      env: { FOREST_URL: 'http://localhost:3001' },
+      nock: nock('http://localhost:3001')
         .get('/api/users/google/robert@gmail.com')
-        .reply(200, { data: { isGoogleAccount: true } }))
-      .input(jwt.sign({}, 'key', { expiresIn: '1day' }), 500)
-      .command(['login', '-e', 'robert@gmail.com'])
-      .it('should display Login successful', (ctx) => {
-        expect(ctx.stdout).to.contain('Login successful');
-      });
+        .reply(200, { data: { isGoogleAccount: true } }),
+      inputs: [
+        `${jwt.sign({}, 'key', { expiresIn: '1day' })}\n`,
+      ],
+      command: () => LoginCommand.run(['-e', 'robert@gmail.com']),
+      outputs: [
+        'Login successful',
+      ],
+    }));
+  });
+
+  describe('with typing email and password', () => {
+    it('should login successful', () => testCli({
+      env: { FOREST_URL: 'http://localhost:3001' },
+      nock: nock('http://localhost:3001')
+        .post('/api/sessions', { email: 'some@mail.com', password: 'pwd' })
+        .reply(200),
+      inputs: [
+        'some@mail.com\n',
+        'pwd\n',
+      ],
+      command: () => LoginCommand.run([]),
+      outputs: [
+        'What is your email address? some@mail.com',
+        'What is your Forest Admin password: [input is hidden] ?',
+        'Login successful',
+      ],
+    }));
   });
 });
---
+++ test/commands/test-cli.js
@@ -0,0 +1,37 @@
+const mockStdin = require('mock-stdin');
+const { stdout, stderr } = require('stdout-stderr');
+const { expect } = require('chai');
+
+module.exports = async function testCli({ nock, env, inputs, command, outputs, errorOutputs }) {
+  const previousEnv = process.env;
+  if (env) process.env = env;
+
+  const stdin = mockStdin.stdin();
+
+  for (let i = 0; i < inputs.length; i++) {
+    setTimeout(() => stdin.send(inputs[i]), 500 + i * 100);
+  }
+
+  stdout.start();
+  stderr.start();
+  await command();
+  nock && nock.done();
+  stdin.end();
+  stdin.reset();
+  stdout.stop();
+  stderr.stop();
+
+  if (outputs) {
+    for (let i = 0; i < outputs.length; i++) {
+      expect(stdout.output).to.contain(outputs[i]);
+    }
+  }
+
+  if (errorOutputs) {
+    for (let i = 0; i < errorOutputs.length; i++) {
+      expect(stderr.output).to.contain(errorOutputs[i]);
+    }
+  }
+
+  process.env = previousEnv;
+};
