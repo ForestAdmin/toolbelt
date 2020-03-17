eab50e91fb155e565b4165af63a368c8e9a549b3
Sliman <smedini@gmail.com>
2020-03-17 23:16:06 +0100

    dialog style

--- test/commands/login.test.js
+++ test/commands/login.test.js
@@ -1,46 +1,43 @@
 const jwt = require('jsonwebtoken');
 const nock = require('nock');
 
-const testCli = require('./test-cli');
+const testDialog = require('./test-cli');
 const LoginCommand = require('../../src/commands/login');
 
 describe('login', () => {
   describe('with email in args', () => {
     describe('with bad token in args', () => {
-      it('should display invalid token', () => testCli({
-        inputs: [
-          `${jwt.sign({}, 'key', { expiresIn: '1day' })}\n`,
-        ],
-        command: () => LoginCommand.run(['-e', 'smile@gmail.com', '-t', 'bad_token']),
-        errorOutputs: [
-          'Invalid token. Please enter your authentication token.',
+      it('should display invalid token', () => testDialog({
+        command: () => LoginCommand.run(['-e', 'smile@gmail.com', '-t', 'invalid_token']),
+        dialog: [
+          { err: 'Invalid token. Please enter your authentication token.' },
         ],
       }));
     });
     describe('with valid token in args', () => {
-      it('should login successful', () => testCli({
-        inputs: [
-          `${jwt.sign({}, 'key', { expiresIn: '1day' })}\n`,
-        ],
+      it('should login successful', () => testDialog({
         command: () => LoginCommand.run(['-e', 'smile@gmail.com', '-t', jwt.sign({}, 'key', { expiresIn: '1day' })]),
-        outputs: [
-          'Login successful',
+        dialog: [
+          { in: `${jwt.sign({}, 'key', { expiresIn: '1day' })}` },
+          { out: 'Login successful' },
         ],
       }));
     });
     describe('with a google mail', () => {
       describe('with a valid token from input', () => {
-        it('should login successful', () => testCli({
+        it('should login successful', () => testDialog({
           env: { FOREST_URL: 'http://localhost:3001' },
+          command: () => LoginCommand.run(['-e', 'robert@gmail.com']),
           nock: nock('http://localhost:3001')
             .get('/api/users/google/robert@gmail.com')
             .reply(200, { data: { isGoogleAccount: true } }),
-          inputs: [
-            `${jwt.sign({}, 'key', { expiresIn: '1day' })}\n`,
-          ],
-          command: () => LoginCommand.run(['-e', 'robert@gmail.com']),
-          outputs: [
-            'Login successful',
+          dialog: [
+            {
+              out: 'To authenticate with your Google account, please follow this link ' +
+                'and copy the authentication token:',
+            },
+            { in: `${jwt.sign({}, 'key', { expiresIn: '1day' })}` },
+            { out: 'Login successful' },
           ],
         }));
       });
@@ -50,57 +47,71 @@ describe('login', () => {
   describe('with typing email', () => {
     describe('with a google mail', () => {
       describe('with a valid token from input', () => {
-        it('should login successful', () => testCli({
+        it('should login successful', () => testDialog({
           env: { FOREST_URL: 'http://localhost:3001' },
+          command: () => LoginCommand.run([]),
           nock: nock('http://localhost:3001')
             .get('/api/users/google/robert@gmail.com')
             .reply(200, { data: { isGoogleAccount: true } }),
-          inputs: [
-            'robert@gmail.com\n',
-            `${jwt.sign({}, 'key', { expiresIn: '1day' })}\n`,
-          ],
-          command: () => LoginCommand.run([]),
-          outputs: [
-            'Login successful',
+          dialog: [
+            { out: 'What is your email address?' },
+            { in: 'robert@gmail.com' },
+            {
+              out: 'To authenticate with your Google account, please follow this link ' +
+                'and copy the authentication token:',
+            },
+            { in: `${jwt.sign({}, 'key', { expiresIn: '1day' })}` },
+            { out: 'Login successful' },
           ],
         }));
       });
     });
     describe('with typing valid password', () => {
-      it('should login successful', () => testCli({
+      it('should login successful', () => testDialog({
         env: { FOREST_URL: 'http://localhost:3001' },
-        nock: nock('http://localhost:3001')
-          .post('/api/sessions', { email: 'some@mail.com', password: 'pwd' })
-          .reply(200),
-        inputs: [
-          'some@mail.com\n',
-          'pwd\n',
-        ],
         command: () => LoginCommand.run([]),
-        outputs: [
-          'What is your email address? some@mail.com',
-          'What is your Forest Admin password: [input is hidden] ?',
-          'Login successful',
+        nock: nock('http://localhost:3001')
+          .post('/api/sessions', { email: 'some@mail.com', password: 'valid_pwd' })
+          .reply(200),
+        dialog: [
+          { out: 'What is your email address?' },
+          { in: 'some@mail.com' },
+          { out: 'What is your Forest Admin password: [input is hidden] ?' },
+          { in: 'valid_pwd' },
+          { out: 'Login successful' },
         ],
       }));
     });
     describe('with typing wrong password', () => {
-      it('should display incorrect password', () => testCli({
+      it('should display incorrect password', () => testDialog({
         env: { FOREST_URL: 'http://localhost:3001' },
+        command: () => LoginCommand.run([]),
         nock: nock('http://localhost:3001')
           .post('/api/sessions', { email: 'some@mail.com', password: 'pwd' })
           .reply(401),
-        inputs: [
-          'some@mail.com\n',
-          'pwd\n',
+        dialog: [
+          { out: 'What is your email address?' },
+          { in: 'some@mail.com' },
+          { out: 'What is your Forest Admin password: [input is hidden] ?' },
+          { in: 'pwd' },
+          { err: 'Incorrect email or password.' },
         ],
+      }));
+    });
+
+    describe('with typing wrong password2', () => {
+      it('should display incorrect password', () => testDialog({
+        env: { FOREST_URL: 'http://localhost:3001' },
         command: () => LoginCommand.run([]),
-        outputs: [
-          'What is your email address? some@mail.com',
-          'What is your Forest Admin password: [input is hidden] ?',
-        ],
-        errorOutputs: [
-          'Incorrect email or password.',
+        nock: nock('http://localhost:3001')
+          .post('/api/sessions', { email: 'some@mail.com', password: 'pwd' })
+          .reply(401),
+        dialog: [
+          { out: 'What is your email address?' },
+          { in: 'some@mail.com' },
+          { out: 'What is your Forest Admin password: [input is hidden] ?' },
+          { in: 'pwd' },
+          { err: 'Incorrect email or password.' },
         ],
       }));
     });
--- test/commands/test-cli.js
+++ test/commands/test-cli.js
@@ -3,15 +3,18 @@ const { stdout, stderr } = require('stdout-stderr');
 const { expect } = require('chai');
 
 module.exports = async function testCli({
-  nock, env, inputs, command, outputs, errorOutputs,
-}) {
+    nock, env, command, dialog,
+  }) {
+  const inputs = dialog ? dialog.filter((type) => type.in).map((type) => type.in) : [];
+  const outputs = dialog ? dialog.filter((type) => type.out).map((type) => type.out) : [];
+  const errorOutputs = dialog ? dialog.filter((type) => type.err).map((type) => type.err) : [];
   const previousEnv = process.env;
   if (env) process.env = env;
 
   const stdin = mockStdin.stdin();
 
   for (let i = 0; i < inputs.length; i += 1) {
-    setTimeout(() => stdin.send(inputs[i]), 500 + i * 100);
+    setTimeout(() => stdin.send(`${inputs[i]}\n`), 500 + i * 100);
   }
 
   stdout.start();
@@ -23,16 +26,12 @@ module.exports = async function testCli({
   stdout.stop();
   stderr.stop();
 
-  if (outputs) {
-    for (let i = 0; i < outputs.length; i += 1) {
-      expect(stdout.output).to.contain(outputs[i]);
-    }
+  for (let i = 0; i < outputs.length; i += 1) {
+    expect(stdout.output).to.contain(outputs[i]);
   }
 
-  if (errorOutputs) {
-    for (let i = 0; i < errorOutputs.length; i += 1) {
-      expect(stderr.output).to.contain(errorOutputs[i]);
-    }
+  for (let i = 0; i < errorOutputs.length; i += 1) {
+    expect(stderr.output).to.contain(errorOutputs[i]);
   }
 
   process.env = previousEnv;
