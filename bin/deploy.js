312fa5b67b0021d8f3aa45d21fe180c55cbced1b
Arnaud Besnier <arnaudibesnier@gmail.com>
2020-05-13 10:13:11 +0200

    chore(release): fix releases without version upgrade

--- bin/deploy.js
+++ bin/deploy.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const { ReleaseManager, ReleaseNoteManager } = require('@forestadmin/devops');
 
 const { DEVOPS_SLACK_TOKEN, DEVOPS_SLACK_CHANNEL } = process.env;
-const OPTIONS = { releaseIcon: '🛠' };
+const OPTIONS = { releaseIcon: '🛠', withVersion: true };
 
 new ReleaseManager(OPTIONS).create()
   .then(() => new ReleaseNoteManager(DEVOPS_SLACK_TOKEN, DEVOPS_SLACK_CHANNEL, OPTIONS).create());
