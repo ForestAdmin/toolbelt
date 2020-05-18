2379db413476bd072128440ebd8b14f56de2a875
Valentin Lamatte <valentinl@forestadmin.com>
2020-05-18 15:38:20 +0200

    feat: add new spinner singleton instance and service for cli commands (#68)

---
+++ src/services/spinner.js
@@ -0,0 +1,126 @@
+const { v4: uuidv4 } = require('uuid');
+const Spinnies = require('spinnies');
+const singletonGetter = require('./singleton-getter');
+
+const spinniesConstructorParameters = {
+  spinnerColor: 'yellow',
+  spinner: {
+    interval: 80,
+    frames: ['⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏'],
+  },
+};
+// NOTICE: Singleton used here to attach all generated spinner to the same spinnies instance.
+const spinniesInstance = singletonGetter(Spinnies, spinniesConstructorParameters);
+
+// NOTICE: Except if you need several spinner running at the same time,
+//         a `singleton-getter` usage should be prefered.
+/**
+ * A single instance of spinner is intended to be used as follow:
+ * @example
+ * const singletonGetter = require('path/to/singleton-getter')
+ * const spinner = singletonGetter(Spinner)
+ *
+ * // synchronously:
+ * spinner.start({ text: 'my super text' })
+ * // do something
+ * spinner.success()
+ *
+ * // on promise:
+ * spinner.start({ text: 'my super text' })
+ * spinner.attachToPromise(mySuperPromise())
+ *
+ * If ever multiple instances are need to be run together:
+ * @example
+ * const Spinner = require('./spinner');
+ *
+ * const spinner1 = new Spinner();
+ * const spinner2 = new Spinner();
+ */
+class Spinner {
+  constructor() {
+    this.spinnies = spinniesInstance;
+    this.currentSpinnerOptions = null;
+  }
+
+  start(options) {
+    if (this.isRunning()) {
+      throw Error('A spinner is already running.');
+    }
+
+    this.currentSpinnerOptions = options;
+    this.currentSpinnerUniqueKey = uuidv4();
+    this.spinnies.add(this.currentSpinnerUniqueKey, options);
+  }
+
+  // NOTICE: optional parameter options to have a custom success message
+  success(options = this.currentSpinnerOptions) {
+    if (!this.isRunning()) {
+      throw Error('No spinner is running.');
+    }
+
+    this.spinnies.succeed(this.currentSpinnerUniqueKey, options);
+    this.stop();
+  }
+
+  // NOTICE: optional parameter options to have a custom fail message
+  fail(options = this.currentSpinnerOptions) {
+    if (!this.isRunning()) {
+      throw Error('No spinner is running.');
+    }
+
+    this.spinnies.fail(this.currentSpinnerUniqueKey, options);
+    this.stop();
+  }
+
+  async pause() {
+    if (!this.isRunning()) {
+      throw Error('No spinner is running.');
+    }
+
+    await this.spinnies.remove(this.currentSpinnerUniqueKey);
+    this.pausedSpinnerOptions = this.currentSpinnerOptions;
+  }
+
+  continue() {
+    if (this.isRunning()) {
+      throw Error('A spinner is already running.');
+    }
+
+    if (!this.pausedSpinnerOptions) {
+      throw Error('No spinner has been paused.');
+    }
+
+    this.spinnies.add(this.currentSpinnerUniqueKey, this.pausedSpinnerOptions);
+    this.pausedSpinnerOptions = null;
+  }
+
+  isRunning() {
+    return !!this.spinnies.pick(this.currentSpinnerUniqueKey);
+  }
+
+  // NOTICE: spinner.start needs to be called first
+  attachToPromise(promise) {
+    if (!this.isRunning()) {
+      throw Error('No spinner is running.');
+    }
+
+    return promise
+      .then((result) => {
+        this.success(this.currentSpinnerOptions);
+        return result;
+      })
+      .catch((error) => {
+        this.fail({ text: error });
+        throw error;
+      });
+  }
+
+  // NOTICE: this stop method should only be used internally to reset the current spinner
+  //         on success or failure.
+  stop() {
+    this.currentSpinnerUniqueKey = null;
+    this.currentSpinnerOptions = null;
+  }
+}
+
+module.exports = Spinner;
---
+++ test/services/spinner.test.js
@@ -0,0 +1,225 @@
+const Spinner = require('../../src/services/spinner');
+
+describe('spinner', () => {
+  describe('running on promise', () => {
+    describe('when promise resolves', () => {
+      const spinnnerOptions = { text: 'valid' };
+      const promise = async () => 'test';
+
+      it('should return the promise value', async () => {
+        expect.assertions(1);
+        const spinner = new Spinner();
+        spinner.start(spinnnerOptions);
+        const result = await spinner.attachToPromise(promise());
+        expect(result).toStrictEqual('test');
+      });
+
+      it('should display the options text', async () => {
+        expect.assertions(1);
+        const spinner = new Spinner();
+        spinner.start(spinnnerOptions);
+        const spy = jest.spyOn(spinner, 'success');
+        await spinner.attachToPromise(promise());
+        expect(spy).toHaveBeenCalledWith(spinnnerOptions);
+      });
+    });
+
+    describe('when promise fails', () => {
+      const spinnnerOptions = { text: 'invalid' };
+      const error = new Error('wrong');
+      const promise = async () => { throw error; };
+
+      // NOTICE: Cannot use jest expect().toThrow() on promises
+      it('should display the error content and throw an error', async () => {
+        expect.assertions(2);
+        const spinner = new Spinner();
+        const spy = jest.spyOn(spinner, 'fail');
+        let message;
+
+        spinner.start(spinnnerOptions);
+
+        try {
+          await spinner.attachToPromise(promise());
+        } catch (e) {
+          message = e.message;
+        }
+
+        expect(message).toBe('wrong');
+        expect(spy).toHaveBeenCalledWith({ text: error });
+      });
+    });
+  });
+
+  describe('when calling pause', () => {
+    const spinnnerOptions = { text: 'trying to pause' };
+
+    describe('when no spinner is running', () => {
+      it('should throw an error', async () => {
+        expect.assertions(1);
+        const spinner = new Spinner();
+        let message;
+        try {
+          await spinner.pause();
+        } catch (error) {
+          message = error.message;
+        }
+        expect(message).toStrictEqual('No spinner is running.');
+      });
+    });
+
+    describe('when a spinner is running', () => {
+      it('should pause the spinner correctly', async () => {
+        expect.assertions(2);
+        const spinner = new Spinner();
+        spinner.start(spinnnerOptions);
+        await spinner.pause();
+        expect(spinner.isRunning()).toBe(false);
+        expect(spinner.pausedSpinnerOptions).toStrictEqual(spinnnerOptions);
+      });
+    });
+  });
+
+  describe('when calling continue', () => {
+    const spinnnerOptions = { text: 'trying to continue' };
+
+    describe('when no spinner is running', () => {
+      describe('when the spinner never started', () => {
+        it('should throw an error', () => {
+          expect.assertions(1);
+          const spinner = new Spinner();
+          expect(() => spinner.continue()).toThrow('No spinner has been paused.');
+        });
+      });
+
+      describe('when the spinner is paused', () => {
+        it('should starts the spinner again', async () => {
+          expect.assertions(2);
+          const spinner = new Spinner();
+          spinner.start(spinnnerOptions);
+          await spinner.pause();
+          spinner.continue();
+          expect(spinner.isRunning()).toBe(true);
+          expect(spinner.pausedSpinnerOptions).toBeNull();
+        });
+      });
+    });
+
+    describe('when a spinner is running', () => {
+      it('should throw an error', () => {
+        expect.assertions(1);
+        const spinner = new Spinner();
+        spinner.start(spinnnerOptions);
+        expect(() => spinner.continue()).toThrow('A spinner is already running.');
+      });
+    });
+  });
+
+  describe('when calling success', () => {
+    const spinnnerOptions = { text: 'trying to success' };
+
+    describe('when no spinner is running', () => {
+      it('should throw an error', () => {
+        expect.assertions(1);
+        const spinner = new Spinner();
+        expect(() => spinner.success()).toThrow('No spinner is running.');
+      });
+    });
+
+    describe('when a spinner has been paused', () => {
+      it('should throw an error', async () => {
+        expect.assertions(1);
+        const spinner = new Spinner();
+        spinner.start(spinnnerOptions);
+        await spinner.pause();
+        expect(() => spinner.success()).toThrow('No spinner is running.');
+      });
+    });
+
+    describe('when a spinner is running', () => {
+      it('should succesfully stop the spinner', () => {
+        expect.assertions(4);
+        const spinner = new Spinner();
+        spinner.start(spinnnerOptions);
+        const spy = jest.spyOn(spinner.spinnies, 'succeed');
+        const spinnerUniqueKey = spinner.currentSpinnerUniqueKey;
+        spinner.success();
+        expect(spy).toHaveBeenCalledWith(spinnerUniqueKey, spinnnerOptions);
+        expect(spinner.isRunning()).toBe(false);
+        expect(spinner.currentSpinnerUniqueKey).toBeNull();
+        expect(spinner.currentSpinnerOptions).toBeNull();
+      });
+
+
+      describe('when a custom success message is given', () => {
+        it('should call success with the message', () => {
+          expect.assertions(4);
+          const spinner = new Spinner();
+          spinner.start(spinnnerOptions);
+          const spy = jest.spyOn(spinner.spinnies, 'succeed');
+          const customOptions = { text: 'par ici' };
+          const spinnerUniqueKey = spinner.currentSpinnerUniqueKey;
+          spinner.success(customOptions);
+          expect(spy).toHaveBeenCalledWith(spinnerUniqueKey, customOptions);
+          expect(spinner.isRunning()).toBe(false);
+          expect(spinner.currentSpinnerUniqueKey).toBeNull();
+          expect(spinner.currentSpinnerOptions).toBeNull();
+        });
+      });
+    });
+  });
+
+
+  describe('when calling fail', () => {
+    const spinnnerOptions = { text: 'trying to fail' };
+
+    describe('when no spinner is running', () => {
+      it('should throw an error', () => {
+        expect.assertions(1);
+        const spinner = new Spinner();
+        expect(() => spinner.fail()).toThrow('No spinner is running.');
+      });
+    });
+
+    describe('when a spinner has been paused', () => {
+      it('should throw an error', async () => {
+        expect.assertions(1);
+        const spinner = new Spinner();
+        spinner.start(spinnnerOptions);
+        await spinner.pause();
+        expect(() => spinner.fail()).toThrow('No spinner is running.');
+      });
+    });
+
+    describe('when a spinner is running', () => {
+      it('should succesfully stop the spinner', () => {
+        expect.assertions(4);
+        const spinner = new Spinner();
+        spinner.start(spinnnerOptions);
+        const spy = jest.spyOn(spinner.spinnies, 'fail');
+        const spinnerUniqueKey = spinner.currentSpinnerUniqueKey;
+        spinner.fail();
+        expect(spy).toHaveBeenCalledWith(spinnerUniqueKey, spinnnerOptions);
+        expect(spinner.isRunning()).toBe(false);
+        expect(spinner.currentSpinnerUniqueKey).toBeNull();
+        expect(spinner.currentSpinnerOptions).toBeNull();
+      });
+
+
+      describe('when a custom fail message is given', () => {
+        it('should call fail with the message', () => {
+          expect.assertions(4);
+          const spinner = new Spinner();
+          spinner.start(spinnnerOptions);
+          const spy = jest.spyOn(spinner.spinnies, 'fail');
+          const customOptions = { text: 'par la' };
+          const spinnerUniqueKey = spinner.currentSpinnerUniqueKey;
+          spinner.fail(customOptions);
+          expect(spy).toHaveBeenCalledWith(spinnerUniqueKey, customOptions);
+          expect(spinner.isRunning()).toBe(false);
+          expect(spinner.currentSpinnerUniqueKey).toBeNull();
+          expect(spinner.currentSpinnerOptions).toBeNull();
+        });
+      });
+    });
+  });
+});
