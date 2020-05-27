3f40f37ca233769b852a8c5cff577ecbb486be44
adriguy <adrien.guyon@gmail.com>
2020-05-27 15:26:30 +0200

    chore(logger): remove winston dependency and use lumber-inspired homemade logger

--- src/services/logger.js
+++ src/services/logger.js
@@ -1,6 +1,36 @@
-const winston = require('winston');
+const chalk = require('chalk');
 
-winston.remove(winston.transports.Console);
-winston.add(winston.transports.Console, { colorize: true });
+class Logger {
+  constructor(silent) {
+    this.silent = silent;
+    this.spinner = null;
+  }
 
-module.exports = winston;
+  log(message, std) {
+    if (!this.silent) {
+      if (std === 'err') {
+        process.stderr.write(message);
+      } else {
+        process.stdout.write(message);
+      }
+    }
+  }
+
+  logLine(color, message, std) {
+    this.log(`${chalk[color]('>')} ${message} \n`, std);
+  }
+
+  logLines(color, messages, std = 'out') {
+    messages.forEach((message) => this.logLine(color, message, std));
+  }
+
+  success(...messages) { this.logLines('green', messages); }
+
+  info(...messages) { this.logLines('blue', messages); }
+
+  warn(...messages) { this.logLines('yellow', messages); }
+
+  error(...messages) { this.logLines('red', messages, 'err'); }
+}
+
+module.exports = new Logger();
