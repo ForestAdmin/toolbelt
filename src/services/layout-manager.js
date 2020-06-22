9cf59781225607b32751c13511d75a7969dc81bf
Raphaël Huchet <raphaelht@gmail.com>
2020-06-22 11:26:02 +0200

    fix: add comments

--- src/services/layout-manager.js
+++ src/services/layout-manager.js
@@ -2,6 +2,11 @@ const agent = require('superagent');
 const authenticator = require('./authenticator');
 const { serverHost } = require('../config');
 
+/**
+ * Deploy layout changes of an environment to production.
+ * @param {Number} environment.id - The environment id that contains the layout changes to deploy.
+ * @param {String} environmentSecret - The current environment secret.
+ */
 function deploy({ id }, environmentSecret) {
   const authToken = authenticator.getAuthToken();
 
