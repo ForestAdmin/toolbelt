0020a8b003e858783a30b2fe986ee1e5d23282a6
Guillaume Deslandes <guillaumed@forestadmin.com>
2020-08-20 15:54:09 +0200

    test: allow creation of subdirectories when mocking files
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- test/commands/test-cli-fs.js
+++ test/commands/test-cli-fs.js
@@ -3,7 +3,10 @@ const fs = require('fs');
 module.exports = {
   mockFile: (file = {}) => {
     const { chdir, name, content } = file;
-    if (chdir) process.chdir(chdir);
+    if (chdir) {
+      fs.mkdirSync(chdir, { recursive: true });
+      process.chdir(chdir);
+    }
     if (name) fs.writeFileSync(name, content);
   },
 
