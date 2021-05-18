783f66e20b5c42ef27c228af07451b3ca0b2dd07
Sliman <smedini@gmail.com>
2021-05-18 17:19:46 +0200

    chore(lint): fixup
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- test/templates/app/server.test.js
+++ test/templates/app/server.test.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 describe('templates > app/server.hbs', () => {
   it('should use the PORT before the APPLICATION_PORT to be sure that the Heroku deployment works well', () => {
     expect.assertions(1);
-    const template = fs.readFileSync(appRoot + '/templates/app/server.hbs').toString();
+    const template = fs.readFileSync(`${appRoot}/templates/app/server.hbs`).toString();
     expect(template).toStrictEqual(expect.stringMatching("process.env.PORT || process.env.APPLICATION_PORT || '3310'"));
   });
 });
