761b7617044dc224d0c4b078f6f8fe5111a7b6db
Guillaume Deslandes <guillaumed@forestadmin.com>
2021-05-20 12:16:33 +0200

    test(coverage): use proper regex patterns in coveragePathIgnorePatterns option
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,16 @@
 module.exports = {
+  collectCoverageFrom: [
+    '**/*.{js,ts,tsx}',
+  ],
+
+  coveragePathIgnorePatterns: [
+    '/coverage',
+    '/.eslint-bin/',
+    '/node_modules/',
+    '/test/',
+    '/test-.*',
+    '/.*.config.js',
+  ],
+
   testEnvironment: 'node',
 };
