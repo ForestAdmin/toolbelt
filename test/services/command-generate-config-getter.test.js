c16ed31b38c6cfdc61a3d5449f1ee0bb82b8110b
Sliman <smedini@gmail.com>
2021-05-25 16:35:41 +0200

    fix: command-generate-config-getter tests

--- test/services/command-generate-config-getter.test.js
+++ test/services/command-generate-config-getter.test.js
@@ -4,8 +4,9 @@ describe('services > command generate config getter', () => {
   describe('with a command with a "connectionUrl" option', () => {
     it('should require [dbConnectionUrl, dbSchema, ssl, mongodbSrv, appName, appHostname, appPort]', () => {
       expect.assertions(1);
-      const commandGenerateConfigGetter = new CommandGenerateConfigGetter({ connectionUrl: 'postgres://forest:secret@localhost:5435/forest' });
-      expect(commandGenerateConfigGetter.getOptions()).toStrictEqual([
+      const options = CommandGenerateConfigGetter
+        .getOptions({ connectionUrl: 'postgres://forest:secret@localhost:5435/forest' });
+      expect(options).toStrictEqual([
         'dbConnectionUrl',
         'dbSchema',
         'ssl',
@@ -21,8 +22,8 @@ describe('services > command generate config getter', () => {
   describe('with a command with no options', () => {
     it('should require [dbDialect, dbName, dbHostname, dbPort, dbUser, dbPassword, dbSchema, email, ssl, mongodbSrv, appName, appHostname, appPort]', () => {
       expect.assertions(1);
-      const commandGenerateConfigGetter = new CommandGenerateConfigGetter({ db: true });
-      expect(commandGenerateConfigGetter.getOptions()).toStrictEqual([
+      const options = CommandGenerateConfigGetter.getOptions({ db: true });
+      expect(options).toStrictEqual([
         'dbDialect',
         'dbName',
         'dbHostname',
