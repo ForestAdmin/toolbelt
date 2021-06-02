6147bf7a3d3b900c6ac3e12983b9258fdeb5ecf9
Guillaume Deslandes <guillaumed@forestadmin.com>
2021-06-02 16:55:53 +0200

    test: wip for schema:update command test
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- test/commands/schema/update.test.js
+++ test/commands/schema/update.test.js
@@ -1,16 +1,34 @@
-const testCli = require('./../test-cli');
+const testCli = require('../test-cli');
 const UpdateCommand = require('../../../src/commands/schema/update');
 
+const {
+  loginValidOidc,
+} = require('../../fixtures/api');
+const { testEnv2 } = require('../../fixtures/env');
+
 describe('schema:update', () => {
-  describe('when the user is not logged in', () => {
-    it.skip('should login the user and then send the schema', () => testCli({
-      commandClass: UpdateCommand,
-      env: {
-        DATABASE_SCHEMA: 'public',
-      },
-      std: [
-        { out: 'this is to be continued' },
-      ],
-    }));
+  describe('login', () => {
+    describe('when the user is not logged in', () => {
+      it('should login the user and then send the schema', () => testCli({
+        commandClass: UpdateCommand,
+        env: testEnv2,
+        api: [
+          () => loginValidOidc(),
+        ],
+        files: [{
+          name: 'package.json',
+          content: 'forest-express-7.0.0',
+        }, {
+          directory: 'forest',
+        }, {
+          directory: 'models',
+        }, {
+          directory: 'routes',
+        }],
+        std: [
+          { out: 'Click on "Log in" on the browser tab which opened automatically or open this link: http://app.localhost/device/check\nYour confirmation code: USER-CODE' },
+        ],
+      }));
+    });
   });
 });
