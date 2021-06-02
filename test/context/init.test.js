ecf882e7e95f435445a7ff07514e33618a6cb118
Guillaume Deslandes <guillaumed@forestadmin.com>
2021-06-02 11:26:24 +0200

    test: fix context init test
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- test/context/init.test.js
+++ test/context/init.test.js
@@ -1,10 +1,10 @@
-const ApplicationContext = require('@forestadmin/context');
-const initContext = require('../../src/context/init');
+const Context = require('@forestadmin/context');
+const contextPlan = require('../../src/context/init');
 
 describe('context > init', () => {
   it('should not throw error with an empty context', () => {
     expect.assertions(1);
 
-    expect(() => initContext(new ApplicationContext())).not.toThrow();
+    expect(() => Context.execute(contextPlan)).not.toThrow();
   });
 });
