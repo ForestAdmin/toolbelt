8aa85b9b27ea039bfb9cdff7f5b859aea940a633
Guillaume Deslandes <guillaumed@forestadmin.com>
2021-06-03 15:18:29 +0200

    test: first steps for schema:update command test
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- test/commands/schema/update.test.js
+++ test/commands/schema/update.test.js
@@ -9,7 +9,7 @@ const { testEnv2 } = require('../../fixtures/env');
 describe('schema:update', () => {
   describe('login', () => {
     describe('when the user is not logged in', () => {
-      it.skip('should login the user and then send the schema', () => testCli({
+      it('should login the user and then send the schema', () => testCli({
         commandClass: UpdateCommand,
         env: testEnv2,
         api: [
@@ -17,7 +17,10 @@ describe('schema:update', () => {
         ],
         files: [{
           name: 'package.json',
-          content: 'forest-express-7.0.0',
+          content: '{ "dependencies": { "forest-express-sequelize": "7.0.0" } }',
+        }, {
+          name: 'config/databases.js',
+          content: 'module.exports = [];',
         }, {
           directory: 'forest',
         }, {
@@ -27,6 +30,36 @@ describe('schema:update', () => {
         }],
         std: [
           { out: 'Click on "Log in" on the browser tab which opened automatically or open this link: http://app.localhost/device/check\nYour confirmation code: USER-CODE' },
+          { spinner: '✅ Connecting to your database(s)' },
+          { spinner: '✅ Analyzing the database(s)' },
+          { spinner: '✅ Generating your files' },
+        ],
+      }));
+    });
+
+    describe('when the user is logged in', () => {
+      it('should send the schema', () => testCli({
+        commandClass: UpdateCommand,
+        env: testEnv2,
+        token: 'any',
+        api: [],
+        files: [{
+          name: 'package.json',
+          content: '{ "dependencies": { "forest-express-sequelize": "7.0.0" } }',
+        }, {
+          name: 'config/databases.js',
+          content: 'module.exports = [];',
+        }, {
+          directory: 'forest',
+        }, {
+          directory: 'models',
+        }, {
+          directory: 'routes',
+        }],
+        std: [
+          { spinner: '✅ Connecting to your database(s)' },
+          { spinner: '✅ Analyzing the database(s)' },
+          { spinner: '✅ Generating your files' },
         ],
       }));
     });
