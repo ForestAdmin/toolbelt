f95646773fd851753d724e78265b676dc2b93096
Sliman <smedini@gmail.com>
2021-06-15 16:24:39 +0200

    chore: wip test code

--- test/services/prompter/inquirer.test.js
+++ test/services/prompter/inquirer.test.js
@@ -71,3 +71,85 @@ const utils = {
     process.stderr.write(stdout.output);
   },
 };
+
+
+const { init, getInstance } = require('@forestadmin/context');
+const initContext = require('../../src/context/init');
+
+init(initContext);
+
+const {
+  mockStd,
+  planifyInputs,
+  assertOutputs,
+  rollbackStd,
+  logStdErr,
+  logStdOut,
+} = utils;
+
+function asArray(any) {
+  if (!any) return [];
+  return Array.isArray(any) ? any : [any];
+}
+
+async function testCli({
+  exitMessage: expectedExitMessage,
+  std: stds,
+  assertNoStdError = true,
+  print = false,
+}) {
+
+  validateInput(file, command, stds, expectedExitCode, expectedExitMessage, rest);
+  const nocks = asArray(api);
+  const inputs = stds ? stds.filter((type) => type.in).map((type) => type.in) : [];
+  const outputs = stds ? stds.filter((type) => type.out).map((type) => type.out) : [];
+  let errorOutputs;
+  if (stds) {
+    // NOTICE: spinnies outputs to std.err
+    errorOutputs = stds
+      .filter((type) => type.err || type.spinner)
+      .map((type) => type.err || type.spinner);
+  } else {
+    errorOutputs = [];
+  }
+
+  mockFile(file);
+  mockEnv(env);
+  mockToken(tokenBehavior);
+  mockDependencies(getInstance());
+  const stdin = mockStd(outputs, errorOutputs, print);
+  planifyInputs(inputs, stdin);
+
+  let actualError;
+  try {
+    try {
+      await command();
+    } finally {
+      rollbackStd(stdin, inputs, outputs);
+    }
+  } catch (error) {
+    actualError = error;
+    if (!expectedExitCode) {
+      logStdErr();
+    }
+  }
+
+  cleanMockedFile(file);
+
+  try {
+    assertNoErrorThrown(actualError, expectedExitCode, expectedExitMessage);
+    assertApi(nocks);
+    assertExitCode(actualError, expectedExitCode);
+    assertExitMessage(actualError, expectedExitMessage);
+    assertOutputs(outputs, errorOutputs, { assertNoStdError });
+  } catch (e) {
+    logStdErr();
+    logStdOut();
+    throw e;
+  }
+
+  rollbackEnv(env);
+  rollbackToken(tokenBehavior);
+}
+
+module.exports = testCli;
