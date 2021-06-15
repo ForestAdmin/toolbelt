bb0633e06106b4d25241b0b63e0a2aa88cb7a8a8
Sliman <smedini@gmail.com>
2021-06-15 16:53:41 +0200

    chore: factorize mockAndSend

--- test/services/prompter/inquirer.test.js
+++ test/services/prompter/inquirer.test.js
@@ -4,11 +4,15 @@ const { stdout } = require('stdout-stderr');
 
 describe('inquirer', () => {
   describe('confirm', () => {
-    it('should confirm', async () => {
-      expect.assertions(2);
+    function mockAndSend(answer) {
       const stdin = mockStdin.stdin();
       stdout.start();
-      setTimeout(() => stdin.send('Y\n'), 100);
+      setTimeout(() => stdin.send(`${answer}\n`), 100);
+    }
+
+    it('should confirm', async () => {
+      expect.assertions(2);
+      mockAndSend('Y');
       const result = await inquirer.prompt([{
         type: 'confirm',
         name: 'The confirm question name',
@@ -18,11 +22,10 @@ describe('inquirer', () => {
       expect(stdout.output).toContain('Is the confirm prompt working?');
       expect(result).toStrictEqual({ 'The confirm question name': true });
     });
+
     it('should not confirm', async () => {
       expect.assertions(2);
-      const stdin = mockStdin.stdin();
-      stdout.start();
-      setTimeout(() => stdin.send('N\n'), 100);
+      mockAndSend('N');
       const result = await inquirer.prompt([{
         type: 'confirm',
         name: 'The confirm question name',
