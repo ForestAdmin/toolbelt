e37a005ec86c67ca7cb59ee0fd4da890b1792f58
Sliman <smedini@gmail.com>
2021-06-15 15:45:44 +0200

    chore: drop one abstract-prompter

--- src/services/projects/create/command-generate-config-getter.js
+++ src/services/projects/create/command-generate-config-getter.js
@@ -1,4 +1,4 @@
-const Prompter = require('../../../utils/prompter/general-prompter');
+const Prompter = require('../../prompter/general-prompter');
 
 const OPTIONS_DATABASE_MANDATORY = [
   'dbDialect',
