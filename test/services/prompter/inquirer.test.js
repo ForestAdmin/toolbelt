69ef22e89a76ef05122d0d1432ea0649b52bd978
Sliman <smedini@gmail.com>
2021-06-16 12:10:41 +0200

    chore: list and password tests

--- test/services/prompter/inquirer.test.js
+++ test/services/prompter/inquirer.test.js
@@ -112,20 +112,82 @@ describe('inquirer', () => {
       expect(result).toStrictEqual({ endpoint: 'same-value' });
     });
     it('should work with invalid validation', async () => {
-      expect.assertions(2);
+      expect.assertions(4);
+      const validateReturns = jest.fn();
       const stdin = startMock();
       const prompt = inquirer.prompt([{
         name: 'endpoint',
         message: 'Enter your local admin backend endpoint:',
         type: 'input',
-        validate: (value) => value === 'same-value',
+        validate: (value) => {
+          validateReturns(value);
+          return value === 'same-value';
+        },
       }]);
       send(stdin, 'other-value');
       send(stdin, 'same-value');
       const result = await prompt;
       stopMock();
+      expect(validateReturns).toHaveBeenNthCalledWith(1, 'other-value');
+      expect(validateReturns).toHaveBeenNthCalledWith(2, 'same-value');
       expect(stdout.output).toContain('Enter your local admin backend endpoint:');
       expect(result).toStrictEqual({ endpoint: 'same-value' });
+    });
+  });
+
+  describe('list', () => {
+    it('should return selected choice with arrow keys', async () => {
+      expect.assertions(2);
+      const arrowDown = '\u001b[B';
+      const enter = '\r';
+
+      const stdin = startMock();
+
+      const prompt = inquirer.prompt([{
+        type: 'list',
+        name: 'dbDialect',
+        message: 'What\'s the database type?',
+        choices: ['postgres', 'mysql', 'mssql', 'mongodb'],
+      }]);
+
+      send(stdin, arrowDown);
+      send(stdin, arrowDown);
+      send(stdin, arrowDown);
+      send(stdin, enter);
+      const result = await prompt;
+      stopMock();
+
+      expect(stdout.output).toContain('What\'s the database type?');
+      expect(result).toStrictEqual({ dbDialect: 'mysql' });
+    });
+  });
+
+  describe('password', () => {
+    it('should work with a password', async () => {
+      expect.assertions(5);
+      const validateReturns = jest.fn();
+      const stdin = startMock();
+
+      const prompt = inquirer.prompt([{
+        type: 'password',
+        name: 'password',
+        message: 'What is your Forest Admin password:',
+        validate: (input) => {
+          validateReturns(input);
+          return input === 'secret' || 'Please enter your password.';
+        },
+      }]);
+
+      send(stdin, 'badsecret');
+      send(stdin, 'secret');
+      const result = await prompt;
+      stopMock();
+
+      expect(validateReturns).toHaveBeenNthCalledWith(1, 'badsecret');
+      expect(validateReturns).toHaveBeenNthCalledWith(2, 'secret');
+      expect(stdout.output).toContain('What is your Forest Admin password:');
+      expect(stdout.output).toContain('Please enter your password.');
+      expect(result).toStrictEqual({ password: 'secret' });
     });
   });
 });
