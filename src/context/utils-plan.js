2a6c5c9ebdcefdcf102818a15f2b04c83736fbfd
Sliman <smedini@gmail.com>
2021-06-22 17:17:54 +0200

    fix: remove terminator sender again

--- src/context/utils-plan.js
+++ src/context/utils-plan.js
@@ -1,10 +1,8 @@
 const KeyGenerator = require('../utils/key-generator');
 const messages = require('../utils/messages');
 const terminator = require('../utils/terminator');
-const terminatorSender = require('../utils/terminator-sender');
 
 module.exports = (plan) => plan
   .addClass(KeyGenerator)
   .addInstance('terminator', terminator)
-  .addInstance('terminatorSender', terminatorSender)
   .addValue('messages', messages);
