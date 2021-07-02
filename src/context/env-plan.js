088a286a7e3fdceeff32161892090770574fcddd
Guillaume Deslandes <guillaumed@forestadmin.com>
2021-07-02 11:31:43 +0200

    fix: add missing env variables to context
    Signed-off-by: Guillaume Deslandes <guillaumed@forestadmin.com>

--- src/context/env-plan.js
+++ src/context/env-plan.js
@@ -8,7 +8,11 @@ const {
 module.exports = (plan) => plan
   .addStep('variables', (planVariables) => planVariables.addValue('env', {
     // ...process.env,
+    DATABASE_SCHEMA: process.env.DATABASE_SCHEMA,
+    DATABASE_URL: process.env.DATABASE_URL,
+    FOREST_ENV_SECRET: process.env.FOREST_ENV_SECRET,
     FOREST_URL: process.env.FOREST_URL || DEFAULT_FOREST_URL,
+    SILENT: process.env.SILENT,
     TOKEN_PATH: process.env.TOKEN_PATH || DEFAULT_TOKEN_PATH,
   }))
   .addStep('others', (planOthers) => planOthers
