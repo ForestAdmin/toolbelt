b0f9c931b06e1552c651fb5b76e45696385ab903
Sliman <smedini@gmail.com>
2021-07-09 14:01:53 +0200

    chore: lazy classes

--- src/context/command-project-create-plan.js
+++ src/context/command-project-create-plan.js
@@ -3,10 +3,10 @@ module.exports = (plan) => plan
   .addModule('Sequelize', () => require('sequelize'))
   .addModule('mongodb', () => require('mongodb'))
   .addModule('Handlebars', () => require('handlebars'))
-  .addUsingClass('database', require('../services/schema/update/database'))
-  .addUsingClass('dumper', require('../services/dumper/dumper'))
-  .addUsingClass('eventSender', require('../utils/event-sender'))
+  .addUsingClass('database', () => require('../services/schema/update/database'))
+  .addUsingClass('dumper', () => require('../services/dumper/dumper'))
+  .addUsingClass('eventSender', () => require('../utils/event-sender'))
   .addValue('GeneralPrompter', require('../services/prompter/general-prompter'))
-  .addUsingClass('commandGenerateConfigGetter', require('../services/projects/create/command-generate-config-getter'))
-  .addUsingClass('projectCreator', require('../services/projects/create/project-creator'))
-  .addUsingClass('spinner', require('../services/spinner'));
+  .addUsingClass('commandGenerateConfigGetter', () => require('../services/projects/create/command-generate-config-getter'))
+  .addUsingClass('projectCreator', () => require('../services/projects/create/project-creator'))
+  .addUsingClass('spinner', () => require('../services/spinner'));
