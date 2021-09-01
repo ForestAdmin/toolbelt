a2269de9bd8e82e3a1c85062f6967678b5765c00
Sliman <smedini@gmail.com>
2021-09-01 16:00:34 +0200

    fix: tests

--- test/commands/environments/create.test.js
+++ test/commands/environments/create.test.js
@@ -42,6 +42,7 @@ describe('environments:create', () => {
         token: 'any',
         commandClass: EnvironmentCreateCommand,
         commandArgs: ['-p', '2', '-n', 'Test', '-u', 'https://test.forestadmin.com', '--format', 'json'],
+        additionnalStep: (plan) => plan.replace('utils/keyGenerator', { generate: () => 'myAuthSecret' }),
         api: [
           () => createEnvironmentValid(),
         ],
@@ -50,6 +51,7 @@ describe('environments:create', () => {
             out: {
               name: 'Test',
               apiEndpoint: 'https://test.forestadmin.com',
+              authSecret: 'myAuthSecret',
               secretKey: '2c38a1c6bb28e7bea1c943fac1c1c95db5dc1b7bc73bd649a0b113713ee29125',
             },
           },
@@ -63,6 +65,7 @@ describe('environments:create', () => {
       env: testEnvWithoutSecret,
       commandClass: EnvironmentCreateCommand,
       commandArgs: ['-n', 'Test', '-u', 'https://test.forestadmin.com'],
+      additionnalStep: (plan) => plan.replace('utils/keyGenerator', { generate: () => 'myAuthSecret' }),
       api: [
         () => loginValidOidc(),
         () => getProjectListValid(),
@@ -88,6 +91,7 @@ describe('environments:create', () => {
         { out: 'ENVIRONMENT' },
         { out: 'name               Test' },
         { out: 'url                https://test.forestadmin.com' },
+        { out: 'FOREST_AUTH_SECRET  myAuthSecret' },
         { out: 'FOREST_ENV_SECRET  2c38a1c6bb28e7bea1c943fac1c1c95db5dc1b7bc73bd649a0b113713ee29125' },
       ],
     }));
