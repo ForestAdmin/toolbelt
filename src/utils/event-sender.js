516856478415cb8b15c62ea8262401c9dccde91b
DrRaider <nsailly@live.fr>
2022-02-21 15:11:10 +0100

    feat: send better events during project generation

--- src/utils/event-sender.js
+++ src/utils/event-sender.js
@@ -31,8 +31,17 @@ class EventSender {
     }
   }
 
-  async notifySuccess() {
-    if (!this.applicationName || !this.command) { return; }
+  async notifySuccess(sessionToken, {
+    projectId, dbDialect, agent, isLocal,
+  }) {
+    if (
+      !this.applicationName
+      || !this.command
+      || !projectId
+      || !dbDialect
+      || !agent
+      || !!isLocal
+    ) { return; }
 
     try {
       await superagent.post(`${this.env.FOREST_URL}/api/lumber/success`, {
@@ -40,10 +49,15 @@ class EventSender {
           type: 'events',
           attributes: {
             command: this.command,
+            project_id: projectId,
             project_name: this.applicationName,
+            db_dialect: dbDialect,
+            agent,
+            is_local: isLocal,
           },
         },
-      });
+      })
+        .set('Authorization', `Bearer ${sessionToken}`);
     } catch (e) {
       // NOTICE: We want silent error because this is just for reporting error
       //         and not not blocking if that does not work.
