7343a33999ec85d280487360bdc074d5db9cf285
Florian Gonzales <fl.gonzales5790@gmail.com>
2022-07-22 11:29:37 +0200

    fix: add context to the new service

--- src/services/ask-for-environment.js
+++ src/services/ask-for-environment.js
@@ -1,17 +1,22 @@
+const Context = require('@forestadmin/context');
+
 const EnvironmentManager = require('./environment-manager');
 
 module.exports = async function askForEnvironment(config, message, availableEnvironmentTypes = []) {
+  const { assertPresent, inquirer } = Context.inject();
+  assertPresent({ inquirer });
+
   const environments = await new EnvironmentManager(config).listEnvironments();
-  const remoteEnvironments = environments.filter(
+  const availableEnvironments = environments.filter(
     (environment) => availableEnvironmentTypes.includes(environment.type),
   );
 
-  if (remoteEnvironments.length) {
-    const response = await this.inquirer.prompt([{
+  if (availableEnvironments.length) {
+    const response = await inquirer.prompt([{
       name: 'environment',
       message,
       type: 'list',
-      choices: remoteEnvironments.map(({ name }) => name),
+      choices: availableEnvironments.map(({ name }) => name),
     }]);
     return response.environment;
   }
