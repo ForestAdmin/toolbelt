0c93d71a8b6f8fc86a9e7de483162fefb3b17900
DrRaider <nsailly@live.fr>
2023-03-23 17:56:01 +0100

    fix(exemple): use the proper method in the exemple

--- test/services/dumpers/agent-nodejs/expected/mssql/index.js
+++ test/services/dumpers/agent-nodejs/expected/mssql/index.js
@@ -37,7 +37,7 @@ const agent = createAgent({
   );
 
 // Add customizations here.
-// agent.addCustomization('collectionName', collection => ...);
+// agent.customizeCollection('collectionName', collection => ...);
 
 agent
   // Expose an HTTP endpoint.
