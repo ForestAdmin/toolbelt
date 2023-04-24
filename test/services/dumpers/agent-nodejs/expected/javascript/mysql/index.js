200130884ca47d58a81a528484c848f5fa05a091
Nicolas SAILLY <nsailly@live.fr>
2023-04-24 10:16:13 +0200

    feat(agent-nodejs): unify javascript and typescript generated code and .env (#589)
    * feat(agent-nodejs): improve js generated js code and .env
    
    * fix: tests

--- test/services/dumpers/agent-nodejs/expected/javascript/mysql/index.js
+++ test/services/dumpers/agent-nodejs/expected/javascript/mysql/index.js
@@ -17,27 +17,69 @@ if (process.env.DATABASE_SSL && JSON.parse(process.env.DATABASE_SSL.toLowerCase(
  * @type {import('@forestadmin/agent').Agent<import('./typings').Schema>}
  */
 const agent = createAgent({
+  // Security tokens
   authSecret: process.env.FOREST_AUTH_SECRET,
   envSecret: process.env.FOREST_ENV_SECRET,
+
+  // Make sure to set NODE_ENV to 'production' when you deploy your project
   isProduction: process.env.NODE_ENV === 'production',
+
   // Autocompletion of collection names and fields
   typingsPath: './typings.ts',
   typingsMaxDepth: 5,
-})
-  // Connect your datasources.
-  .addDataSource(
-    createSqlDataSource({
-      uri: process.env.DATABASE_URL,
-      schema: process.env.DATABASE_SCHEMA,
-      dialectOptions,
-    }),
-  );
+});
+
+// Connect your datasources
+// All options are documented at https://docs.forestadmin.com/developer-guide-agents-nodejs/data-sources/connection
+agent.addDataSource(
+  createSqlDataSource({
+    uri: process.env.DATABASE_URL,
+    schema: process.env.DATABASE_SCHEMA,
+    dialectOptions,
+  }),
+);
 
 // Add customizations here.
-// agent.customizeCollection('collectionName', collection => ...);
+// For instance, you can code custom actions, charts, create new fields or relationships, load plugins.
+// As your project grows, you will need to split it into multiple files!
+//
+// Here is some code to get your started
+//
+// agent.customizeCollection('products', collection => {
+//   // Actions are documented here:
+//   // https://docs.forestadmin.com/developer-guide-agents-nodejs/agent-customization/actions
+//   collection.addAction('Order new batch from supplier', {
+//     scope: 'Single', // This action can be triggered product by product
+//     form: [{ label: 'Quantity', type: 'Number', isRequired: true }],
+//     execute: async (context, resultBuilder) => {
+//       const product = await context.getRecord(['id', 'name'])
+//       const quantity = context.formValues['Quantity'];
 
-agent
-  // Expose an HTTP endpoint.
-  .mountOnStandaloneServer(process.env.PORT || process.env.APPLICATION_PORT)
-  // Start the agent.
-  .start();
+//       // ... Perform work here ...
+
+//       return resultBuilder.success(`Your order for a batch of ${quantity} '${product.name}' was sent`);
+//     }
+//   });
+
+//   // Search customization is documented here:
+//   // https://docs.forestadmin.com/developer-guide-agents-nodejs/agent-customization/search
+//   collection.replaceSearch(searchString => {
+//     // user has typed a product id, let's only that column
+//     if (searchString.match(/^prdid[\d]{8}/$))
+//       return { field: 'id', operator: 'Equal', value: searchString };
+
+//     // Otherwise assume that user wants to search for a product by name
+//     return { field: 'name', operator: 'Contains', value: searchString };
+//   });
+// });
+
+// Expose an HTTP endpoint.
+agent.mountOnStandaloneServer(Number(process.env.APPLICATION_PORT));
+
+// Start the agent.
+agent.start().catch(error => {
+  console.error('\x1b[31merror:\x1b[0m Forest Admin agent failed to start\n');
+  console.error('');
+  console.error(error.stack);
+  process.exit(1);
+});
