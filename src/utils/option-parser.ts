b810f647b0b79f0df29cf7cb37435eed197eff7e
Romain Gilliotte <rgilliotte@gmail.com>
2023-05-25 16:50:58 +0200

    fix: remove useless export

--- src/utils/option-parser.ts
+++ src/utils/option-parser.ts
@@ -21,7 +21,7 @@ export type CommandOptions<T = Record<string, unknown>> = {
 };
 
 /** Query options interactively */
-export async function getInteractiveOptions<T>(
+async function getInteractiveOptions<T>(
   options: CommandOptions,
   values: Record<string, unknown> = {},
 ): Promise<T> {
