e940447ce76d2a0ca4708c293e0b2a897675e136
SteveBunlon <SteveBunlon@users.noreply.github.com>
2023-12-07 11:26:26 +0100

    fix(dumper): ignore mongodb fields that includes column and log warning (#652)

--- test/services/dumpers/agent-nodejs/agent-nodejs-dumper-mongoose-models.test.ts
+++ test/services/dumpers/agent-nodejs/agent-nodejs-dumper-mongoose-models.test.ts
@@ -6,6 +6,7 @@ import rimraf from 'rimraf';
 import defaultPlan from '../../../../src/context/plan';
 import AgentNodeJsDumper from '../../../../src/services/dumpers/agent-nodejs';
 import languages from '../../../../src/utils/languages';
+import deepNestedColumn from '../../analyzer/expected/mongo/db-analysis-output/deep-nested-fields-column.expected.json';
 import deepNested from '../../analyzer/expected/mongo/db-analysis-output/deep-nested-fields.expected.json';
 import hasMany from '../../analyzer/expected/mongo/db-analysis-output/hasmany.expected.json';
 import manyObjectIdFields from '../../analyzer/expected/mongo/db-analysis-output/many-objectid-fields.expected.json';
@@ -36,9 +37,16 @@ describe('services > dumpers > agentNodejsDumper > mongoose models', () => {
       language,
     };
 
-    const injectedContext = Context.execute(defaultPlan);
+    const injectedContext = Context.execute(defaultPlan) as any;
+
+    const loggerWarnSpy = jest.spyOn(injectedContext.logger, 'warn');
+
     const dumper = new AgentNodeJsDumper(injectedContext);
     await dumper.dump(config, schema);
+
+    return {
+      loggerWarnSpy,
+    };
   }
 
   describe.each([languages.Javascript, languages.Typescript])('language: $name', language => {
@@ -98,6 +106,14 @@ describe('services > dumpers > agentNodejsDumper > mongoose models', () => {
               expectedFilePath: `${__dirname}/expected/${language.name}/mongo-models/sub-documents-with-ids.expected.${language.fileExtension}`,
             },
           },
+          {
+            name: 'should not dump any fields containing column',
+            schema: deepNestedColumn,
+            file: {
+              model: 'persons',
+              expectedFilePath: `${__dirname}/expected/${language.name}/mongo-models/deep-nested-column.expected.${language.fileExtension}`,
+            },
+          },
         ];
 
         it.each(testCases)(`$name`, async ({ schema, file }) => {
@@ -114,6 +130,25 @@ describe('services > dumpers > agentNodejsDumper > mongoose models', () => {
           );
 
           expect(generatedFile).toStrictEqual(expectedFile);
+        });
+
+        it('should warn information when a field containing column has been ignored', async () => {
+          expect.assertions(4);
+
+          rimraf.sync(`${appRoot}/test-output/${language.name}/mongodb/`);
+
+          const { loggerWarnSpy } = await dump(language, deepNestedColumn);
+
+          expect(loggerWarnSpy).toHaveBeenCalledTimes(3);
+          expect(loggerWarnSpy).toHaveBeenCalledWith(
+            'Ignoring field name:column from collection persons as it contains column and is not valid.',
+          );
+          expect(loggerWarnSpy).toHaveBeenCalledWith(
+            'Ignoring field answer:column from collection persons as it contains column and is not valid.',
+          );
+          expect(loggerWarnSpy).toHaveBeenCalledWith(
+            'Ignoring field so:column from collection persons as it contains column and is not valid.',
+          );
         });
       });
     });
